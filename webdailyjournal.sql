f718893daa20f477b7b609a3d972224b56c0b4be
suryaanandapramudya <suryaanandapramudya@gmail.com>
2026-01-08 19:53:53 +0700

    add file

--- webdailyjournal.sql
+++ webdailyjournal.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 30, 2025 at 05:13 PM
+-- Generation Time: Dec 8, 2026 at 19.53 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
